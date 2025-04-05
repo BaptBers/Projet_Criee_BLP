ddf4eef942bf34de96831d21b778f48c52a7348f
Mohamed Laaraj <mohamedlaaraj.67380@gmail.com>
2025-04-05 15:24:08 +0200

    Début changement style accueil

--- siteCriee/application/views/accueil.php
+++ siteCriee/application/views/accueil.php
@@ -1,4 +1,6 @@
-	<body>
-		<br>
-		<h3>Bienvenue sur le site de la criée de Cornouailles !</h3>
-	</body>
+
+<body class="accueil"> <!-- Assure-toi que la classe "accueil" est bien ici -->
+    <br><br>
+    <h3>Bienvenue sur le site de la criée de Cornouailles !</h3>
+</body>
+</html>
