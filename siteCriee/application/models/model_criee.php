eed0eb6182fd3c07772e53447272d33a74a3a83c
BaptBers <b.bersuder2605@gmail.com>
2025-04-27 12:34:05 +0200

    version finale du site

--- siteCriee/application/models/model_criee.php
+++ siteCriee/application/models/model_criee.php
@@ -23,27 +23,27 @@ class Model_criee extends CI_Model
         return $query_result; 
     }
 
-    public function setLot($IdBateau, $IdEspece, $datePeche, $IdTaille, $IdPresentation, $IdBac, $IdQualite, $PoidsBrutLot, $prixDepart, $prixEnchereActuelle, $dateOuverture, $dateFin, $heureOuverture, $heureFin, $idAdmin)
+    public function setLot($IdBateau, $datePeche, $IdEspece, $IdTaille, $IdPresentation, $IdBac, $IdQualite, $PoidsBrutLot, $prixDepart, $prixEnchereActuelle, $dateOuverture, $dateFin, $heureOuverture, $heureFin, $idAdmin)
     {
         $search = "INSERT INTO lot (
-            IdBateau, IdEspece, datePeche, IdTaille, IdPresentation, IdBac, IdQualite, IdImage, 
+            IdBateau, datePeche, IdEspece, IdTaille, IdPresentation, IdBac, IdQualite, IdImage, 
             poidsBrutLot, prixDepart, prixEnchereActuelle, dateOuverture, dateFin, 
             heureOuverture, heureFin, idAdmin
         ) VALUES (
-            :IdBateau, :IdEspece, :datePeche, :IdTaille, :IdPresentation, :IdBac, :IdQualite, :IdImage, 
+            :IdBateau, :datePeche, :IdEspece, :IdTaille, :IdPresentation, :IdBac, :IdQualite, :IdImage, 
             :PoidsBrutLot, :prixDepart, :prixEnchereActuelle, :dateOuverture, :dateFin, 
             :heureOuverture, :heureFin, :idAdmin
         )";
     
         $result = $this->db->conn_id->prepare($search);
         $result->bindParam(':IdBateau', $IdBateau, PDO::PARAM_INT);
-        $result->bindParam(':IdEspece', $IdEspece, PDO::PARAM_INT);
         $result->bindParam(':datePeche', $datePeche, PDO::PARAM_STR);
+        $result->bindParam(':IdEspece', $IdEspece, PDO::PARAM_INT);     
         $result->bindParam(':IdTaille', $IdTaille, PDO::PARAM_INT);
         $result->bindParam(':IdPresentation', $IdPresentation, PDO::PARAM_INT);
         $result->bindParam(':IdBac', $IdBac, PDO::PARAM_INT);
         $result->bindParam(':IdQualite', $IdQualite, PDO::PARAM_INT);
-        $result->bindParam(':IdImage', $IdEspece, PDO::PARAM_INT); // si IdImage = IdEspece
+        $result->bindParam(':IdImage', $IdEspece, PDO::PARAM_INT); 
         $result->bindParam(':PoidsBrutLot', $PoidsBrutLot, PDO::PARAM_STR);
         $result->bindParam(':prixDepart', $prixDepart, PDO::PARAM_STR);
         $result->bindParam(':prixEnchereActuelle', $prixEnchereActuelle, PDO::PARAM_STR);
@@ -54,38 +54,19 @@ class Model_criee extends CI_Model
         $result->bindParam(':idAdmin', $idAdmin, PDO::PARAM_INT);
     
         $result->execute();
-        // return true;
-
-        $lastId = $this->db->conn_id->lastInsertId();
-
-        // Vérifier l'insertion
-        $checkQuery = "SELECT * FROM lot WHERE IdLot = :id";
-        $checkStmt = $this->db->conn_id->prepare($checkQuery);
-        $checkStmt->bindParam(':id', $lastId, PDO::PARAM_INT);
-        $checkStmt->execute();
-        $insertedData = $checkStmt->fetch(PDO::FETCH_ASSOC);
-
-        return $insertedData;
     }    
 
     public function setPeche($idBateau, $datePeche)
     {
         $query = "INSERT INTO peche (IdBateau, datePeche) VALUES (:idBateau, :datePeche)";
-        
         $stmt = $this->db->conn_id->prepare($query);
         $stmt->bindParam(':idBateau', $idBateau, PDO::PARAM_INT);
         $stmt->bindParam(':datePeche', $datePeche, PDO::PARAM_STR);
-    
-        if ($stmt->execute()) {
-            return "Insertion réussie ! Nombre de lignes affectées : " . $stmt->rowCount();
-        } else {
-            $error = $stmt->errorInfo();
-            return "Erreur d'insertion : " . $error[2]; // message d'erreur
-        }
+        $stmt->execute();
     }
     
     public function setFactureEntete($dateCommande, $heureCommande, $montantTotal, $idAcheteur) {
-        $sql = "INSERT INTO FACTURE (dateCommande, heureCommande, montantTotal, IdAcheteur)
+        $sql = "INSERT INTO facture (dateCommande, heureCommande, montantTotal, IdAcheteur)
                 VALUES (:dateCommande, :heureCommande, :montantTotal, :idAcheteur)";
         
         try {
@@ -217,7 +198,7 @@ class Model_criee extends CI_Model
     }
 
     public function getAcheteur($idLot) {
-        $sql = "SELECT * FROM lot, bateau, espece, taille, bac, qualite, image, presentation, historique_encheres, acheteur WHERE historique_encheres.IdAcheteur = acheteur.IdAcheteur AND lot.IdLot = historique_encheres.IdLot AND lot.IdAcheteur = historique_encheres.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND bac.IdBac = lot.IdBac AND qualite.IdQualite = lot.IdQualite AND image.IdImage = lot.idImage AND presentation.IdPresentation = lot.IdPresentation AND lot.IdLot = ?";
+        $sql = "SELECT historique_encheres.IdAcheteur, acheteur.login FROM lot, bateau, espece, taille, bac, qualite, image, presentation, historique_encheres, acheteur WHERE historique_encheres.IdAcheteur = acheteur.IdAcheteur AND lot.IdLot = historique_encheres.IdLot AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND bac.IdBac = lot.IdBac AND qualite.IdQualite = lot.IdQualite AND image.IdImage = lot.idImage AND presentation.IdPresentation = lot.IdPresentation AND lot.IdLot = ? ORDER BY historique_encheres.montantEnchere DESC";
         $query = $this->db->query($sql, [$idLot]);
         return $query->row_array();
     }
@@ -226,18 +207,18 @@ class Model_criee extends CI_Model
         $sql = "UPDATE lot 
                 SET statut = 'ouverte' 
                 WHERE statut = 'future' 
-                AND CONCAT(dateOuverture, ' ', heureOuverture) <= NOW()";
+                AND TIMESTAMP(dateOuverture, heureOuverture) <= NOW()";
         $this->db->query($sql);
     }
-
+      
     public function updateEncheresExpirees() {
         $sql = "UPDATE lot 
                 SET statut = 'fermee' 
                 WHERE statut = 'ouverte' 
-                AND CONCAT(dateFin, ' ', heureFin) <= NOW()";
+                AND TIMESTAMP(dateFin, heureFin) <= NOW()";
         $this->db->query($sql);
-    }
-
+    }    
+       
     public function getLotsFermes()
     {
         $search = "SELECT * FROM lot, bateau, espece, taille, bac, qualite, image, presentation WHERE bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND bac.IdBac = lot.IdBac AND qualite.IdQualite = lot.IdQualite AND image.IdImage = lot.idImage AND presentation.IdPresentation = lot.IdPresentation AND statut = 'fermee'";
@@ -249,25 +230,43 @@ class Model_criee extends CI_Model
     }
 
     public function enregistreEnchere($idLot, $idAcheteur, $montantEnchere) {
-        // Vérifier si l'enchère actuelle est inférieure au montant proposé
-        $sql = "SELECT prixEnchereActuelle FROM lot WHERE IdLot = ?";
+        // Vérifier si l'enchère est encore ouverte et pas expirée
+        $sql = "SELECT prixEnchereActuelle, statut, TIMESTAMP(dateFin, heureFin) AS finEnchere
+                FROM lot 
+                WHERE IdLot = ?";
         $query = $this->db->query($sql, [$idLot]);
         $lotDetails = $query->row_array();
-        
-        if ($lotDetails && $lotDetails['prixEnchereActuelle'] < $montantEnchere) {
-            // Si le montant proposé est supérieur, mettre à jour l'enchère
-            $updateSql = "UPDATE lot SET prixEnchereActuelle = ?, IdAcheteur = ? WHERE IdLot = ?";
-            $this->db->query($updateSql, [$montantEnchere, $idAcheteur, $idLot]);
-            
-            // Insérer l'enchère dans l'historique des enchères
-            $insertSql = "INSERT INTO historique_encheres (IdLot, IdAcheteur, montantEnchere, dateEnchere) VALUES (?, ?, ?, NOW())";
-            $this->db->query($insertSql, [$idLot, $idAcheteur, $montantEnchere]);
-            
-            return true;  // L'enchère a été placée avec succès
+    
+        if ($lotDetails) {
+            // Vérifier si l'enchère est encore ouverte
+            if ($lotDetails['statut'] != 'ouverte') {
+                return false; 
+            }
+    
+            // Vérifier si la fin est dépassée
+            if (strtotime($lotDetails['finEnchere']) <= time()) {
+                return false;
+            }
+    
+            // Vérifier que le montant est supérieur à l'enchère actuelle
+            if ($lotDetails['prixEnchereActuelle'] < $montantEnchere) {
+                // Mise à jour du prix et insertion dans l'historique
+                $updateSql = "UPDATE lot SET prixEnchereActuelle = ? WHERE IdLot = ?";
+                $this->db->query($updateSql, [$montantEnchere, $idLot]);
+    
+                $insertSql = "INSERT INTO historique_encheres (IdLot, IdAcheteur, montantEnchere, dateEnchere) 
+                              VALUES (?, ?, ?, NOW())";
+                $this->db->query($insertSql, [$idLot, $idAcheteur, $montantEnchere]);
+    
+                return true;
+            } else {
+                return false; // Montant trop bas
+            }
         } else {
-            return false; // L'enchère n'est pas valable
+            return false; // Lot non trouvé
         }
     }
+    
 
     public function getInfosPanier($idAcheteur){
         // Définir la requête SQL pour récupérer les informations du panier de l'acheteur
@@ -302,7 +301,7 @@ class Model_criee extends CI_Model
     }
 
     public function getNomScientifiques() {
-        $sql = "SELECT IdEspece, nomScientifique FROM ESPECE"; // Table ESPECE
+        $sql = "SELECT IdEspece, nomScientifique FROM espece"; // Table ESPECE
         $result = $this->db->query($sql); // Exécution de la requête avec CodeIgniter DB
     
         // Remplacez fetchAll() par result_array() pour obtenir les résultats sous forme de tableau
@@ -311,14 +310,14 @@ class Model_criee extends CI_Model
 
     // Fonction pour récupérer les noms communs (de la table ESPECE)
     public function getNomCommuns() {
-        $sql = "SELECT IdEspece, nomCommun FROM ESPECE"; // Table ESPECE
+        $sql = "SELECT IdEspece, nomCommun FROM espece"; // Table ESPECE
         $result = $this->db->query($sql);
         return $result->result_array();
     }
 
     // Fonction pour récupérer les noms de bateaux (de la table BATEAU)
     public function getNomBateaux() {
-        $sql = "SELECT IdBateau, nomBateau FROM BATEAU"; // Table BATEAU
+        $sql = "SELECT IdBateau, nomBateau FROM bateau"; // Table BATEAU
         $result = $this->db->query($sql);
         return $result->result_array();
     }
