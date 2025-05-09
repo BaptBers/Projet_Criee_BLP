d589bb91088a8f62b72b0fc3669c22a300f3cc50
BaptBers <b.bersuder2605@gmail.com>
2025-05-09 11:07:18 +0200

    site final version production

--- siteCriee/application/views/echec.php
+++ siteCriee/application/views/echec.php
@@ -1,7 +1,7 @@
 <body>
     <br>
     <div class ="resultat">
-        <p id="fail">Échec de l'enregistrement de l'enchère. Vérifiez bien que l'enchère soit <strong>STRICTEMENT</strong> supérieure à celle actuelle.</p>
+        <p id="fail">Échec de l'enregistrement de l'enchère. Vérifiez bien que l'enchère soit <strong>STRICTEMENT</strong> supérieure à celle actuelle, ou que l'enchère ne soit pas terminée.</p>
     </div>
     <div class="bouton-retour">
         <a href="<?php echo site_url('welcome/contenu/EncheresEnCours'); ?>" class="buttonRetour">Retour</a>
